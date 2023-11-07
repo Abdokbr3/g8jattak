fad294d9f67370fb7ca9decaa6ef749770ab910a
Abdokbr3 <96750012+Abdokbr3@users.noreply.github.com>
2023-11-07 20:51:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ python = lambda : os.system('apt install python')
 python()
 python3 = lambda : os.system('apt install python3')
 python3()
+requests = lambda : os.system('pip install requests')
+requests()
 clear = lambda : os.system('clear')
 clear()
 print('''
